caa507b36fb27d95940991dfe0c6d8bb9cf34cbc
Manuel Silenzi <manuelsilenzi1@gmail.com>
2021-06-17 19:50:38 -0300

    add ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,21 @@
 const CopyPlugin = require('copy-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
-
-
-module.exports = (env, {mode}) => {
+module.exports = (env, { mode }) => {
   const BUILD_PATH = (mode === 'production') ? 'docs' : 'build';
 
-  console.log('ENV:', env);
-  console.log('MODE:', mode);
-  console.log();
-
   return {
-    entry: { 
+    entry: {
       index: path.resolve(__dirname, 'src/js', 'index.js'),
     },
-  
-  
     output: {
       clean: true,
-      path: path.resolve(__dirname, BUILD_PATH)
+      path: path.resolve(__dirname, BUILD_PATH),
     },
-  
-  
+
     module: {
       rules: [
         {
@@ -32,29 +24,30 @@ module.exports = (env, {mode}) => {
         },
         {
           test: /\.scss$/i,
-          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
         },
         {
           test: /\.js$/,
           exclude: /node_modules/,
-          use: ["babel-loader"]
+          use: ['babel-loader'],
         },
-      ]
+      ],
     },
-  
-  
+
     plugins: [
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, 'src', 'index.html'),
       }),
-  
+
       new MiniCssExtractPlugin(),
 
       new CopyPlugin({
         patterns: [
-          {from: 'src/assets', to: 'assets'}
-        ]
-      })
+          { from: 'src/assets', to: 'assets' },
+        ],
+      }),
+
+      new ESLintPlugin(),
     ],
-  }
-}
+  };
+};
